5bf3bdcdbe9d2054686427cb11a0333baf8c7d64
Andrew Cookro <andrew.cookro@gmail.com>
2017-08-30 19:23:55 -0400

    Add a tooltip to explain the the identicon theme change function. Currently uses browsers native alert window, but should be changed to css in the future where it will appear and disappear without having to click OK.

--- public/components/nav.js
+++ public/components/nav.js
@@ -18,6 +18,8 @@ class Nav extends React.Component {
         this.props.updateIdenticon(this.state.value);
         this.setState({value: ''});
       });
+    } else {
+      alert('GitHub username field must not be empty.');
     }
   }
 
@@ -25,6 +27,9 @@ class Nav extends React.Component {
     this.setState({value: event.target.value});
   }
 
+  handleTooltipClick() {
+    alert('A GitHub username is required to change the identicon theme.\n\nAn identicon is a hash of your username which GitHub uses as your default profile pic. Input your own username to see your unique identicon.');
+  }
 
   render() {
     return (
@@ -36,10 +41,11 @@ class Nav extends React.Component {
         </div>
 
         <div className='input-area'>
-          <div className='theme'><span>identicon: {this.state.currentTheme}</span></div>
+          <div className='theme'><span>identicon theme: {this.state.currentTheme}</span></div>
           <div className='input-bar'>
             <form onSubmit={this.handleSubmit.bind(this)} autocomplete='nope'>
-              <input className='githubUsernameField' name='githubUsername' type='text' value={this.state.value} onChange={this.handleChange.bind(this)} placeholder='Input your github username' autocomplete='nope'/>
+              <span className="glyphicon glyphicon-question-sign" onMouseEnter={this.handleTooltipClick}></span>
+              <input className='githubUsernameField' name='githubUsername' type='text' value={this.state.value} onChange={this.handleChange.bind(this)} placeholder='Input a GitHub username' autocomplete='nope'/>
               <input className='githubUsernameButton btn btn-sm' type='submit' value='Submit' />
             </form>
           </div>
